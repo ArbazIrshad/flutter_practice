6ac895d71831a53f64c9f9fc749415b0bcd67db6
Arbaz Irshad <Arbazirshad25@gmail.com>
2023-08-07 02:34:34 +0500

    Inital Commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AnimationBasicsApp());
+}
+
+class AnimationBasicsApp extends StatelessWidget {
+  const AnimationBasicsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Animation Basics',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomPaint(
+        painter: SquareCustomPainter(),
+        child: Column(
+          children: [
+            Text('Hello from the other side')
+            // Expanded(
+            //     child: Container(
+            //         // color: Colors.teal,
+            //         ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SquareCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()..color = const Color(0xFF006600);
+    final path = Path();
+
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height / 2);
+    path.lineTo(0, size.height / 2);
+    path.close();
+
+    canvas.drawPath(path, paint);
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), size.width / 4,
+        Paint()..color = Colors.yellow);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
